be7e4230ecfbd9a96aa3802949fa275f031c49c2
Sheikh Suhail <suhailraza95@gmail.com>
2026-06-02 00:52:52 +0530

    Code refactor and API docs

--- app.js
+++ app.js
@@ -1,4 +1,54 @@
 require('dotenv').config();
+<<<<<<< HEAD
+
+const express = require('express');
+const connectDB = require('./config/db');
+const morgan = require("morgan");
+
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument =
+  YAML.load("./docs/openapi.yaml");
+
+const redoc = require("redoc-express");
+ 
+
+const authRoutes = require('./routes/authRoutes');
+const otpRoutes = require('./routes/otpRoutes');
+const passwordRoutes = require('./routes/passwordRoutes');
+
+
+
+const errorMiddleware = require('./middleware/errorMiddleware');
+const apiKeyMiddleware = require('./middleware/apiKeyMiddleware');
+
+const app = express();
+app.use(morgan("tiny"));
+// CONNECT DATABASE
+connectDB();
+
+// REQUIRED FOR req.body
+app.use(express.json());
+app.use(
+  "/docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument)
+);
+
+app.get(
+  "/redoc",
+  redoc({
+    title: "JWT Login System API",
+    specUrl: "/openapi.yaml"
+  })
+);
+app.get("/openapi.yaml", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "docs/openapi.yaml")
+  );
+});
+
+=======
 
 const express = require('express');
 const connectDB = require('./config/db');
@@ -23,6 +73,7 @@ connectDB();
 app.use(express.json());
 
 
+>>>>>>> 356318497d0545274308fe65225ebde45f729e64
 // ROUTES
 app.use('/api',apiKeyMiddleware, authRoutes);
 app.use('/api',apiKeyMiddleware, otpRoutes);
